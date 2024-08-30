abf5cfc70a440de26a9c1f0ab39ed55404c3c351
reine-ishyanami <2402979195@qq.com>
2024-08-30 09:16:07 +0800

    fix: input area flow

--- gemini/src/model.rs
+++ gemini/src/model.rs
@@ -1,10 +1,12 @@
 #![allow(dead_code)]
 
+use chrono::{DateTime, Local};
+
 #[derive(Debug, Clone)]
 pub struct ChatMessage {
     pub message: String,
     pub sender: Sender,
-    pub timestamp: u64,
+    pub date_time: DateTime<Local>,
 }
 
 #[derive(Debug, Clone)]
--- gemini/src/ui.rs
+++ gemini/src/ui.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use chrono::Local;
 use gemini_api::model::blocking::Gemini;
 use gemini_api::model::LanguageModel;
 use ratatui::layout::Alignment;
@@ -16,7 +17,6 @@ use ratatui::{
     DefaultTerminal,
 };
 use std::sync::mpsc::{self, Receiver, Sender};
-use std::time::{SystemTime, UNIX_EPOCH};
 
 use crate::model::ChatMessage;
 use crate::model::Sender::{Bot, User};
@@ -39,13 +39,22 @@ impl From<&ChatMessage> for ListItem<'_> {
                 let message = value.message.clone();
                 let message_lines = message.split("\n");
                 let mut lines = Vec::new();
+                let mut line_width = 0;
                 for line in message_lines {
+                    if line_width == 0 {
+                        line_width = line.len();
+                    }
                     lines.push(
-                        Line::from(line.to_string())
+                        Line::from(format!("{:width$}", line, width = line_width))
                             .alignment(Alignment::Right)
                             .style(Style::default().fg(Color::Green)),
                     );
                 }
+                lines.push(
+                    Line::from(value.date_time.format("%H:%M:%S").to_string())
+                        .alignment(Alignment::Right)
+                        .style(Style::default().fg(Color::Blue)),
+                );
                 lines
             }
             Bot => {
@@ -59,6 +68,11 @@ impl From<&ChatMessage> for ListItem<'_> {
                             .style(Style::default().fg(Color::Cyan)),
                     );
                 }
+                lines.push(
+                    Line::from(value.date_time.format("%H:%M:%S").to_string())
+                        .alignment(Alignment::Left)
+                        .style(Style::default().fg(Color::Blue)),
+                );
                 lines
             }
         };
@@ -87,7 +101,7 @@ impl UI {
                 self.chat_history.push(ChatMessage {
                     sender: Bot,
                     message,
-                    timestamp: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs(),
+                    date_time: Local::now(),
                 });
                 self.receiving_message = false;
             }
@@ -111,7 +125,7 @@ impl UI {
                         self.chat_history.push(ChatMessage {
                             sender: User,
                             message: self.input_buffer.clone(),
-                            timestamp: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs(),
+                            date_time: Local::now(),
                         });
                     }
                     self.input_buffer.clear();
@@ -128,6 +142,8 @@ impl UI {
                     self.cursor_pos += 1;
                 }
             }
+            event::KeyCode::Up => {}
+            event::KeyCode::Down => {}
             event::KeyCode::Home => {
                 self.cursor_pos = 0;
             }
@@ -166,6 +182,12 @@ impl UI {
 
 impl Widget for &mut UI {
     fn render(self, area: Rect, buf: &mut Buffer) {
+        // 计算显示区域宽度
+        let chat_area_width = || area.width as usize - 2 - 5;
+        // 这里 -2 的原因是因为输入框中具有两侧的的 1px 边框
+        // 计算输入框区域宽度
+        let input_area_width = || area.width as usize - 2;
+
         let input_block_title = if self.gemini.is_none() {
             "Input Key"
         } else {
@@ -176,8 +198,17 @@ impl Widget for &mut UI {
             .title("Chat")
             .border_style(Style::default().fg(Color::Blue))
             .borders(Borders::ALL);
-        let input_block = Block::default().title(input_block_title).borders(Borders::ALL);
-        let input_paragraph = Paragraph::new(self.input_buffer.as_str())
+        let input_block = Block::default()
+            .title(input_block_title)
+            .border_style(Style::default().fg(Color::Green))
+            .borders(Borders::ALL);
+        // 输入框内容
+        let text = if self.input_buffer.len() > input_area_width() && self.cursor_pos > input_area_width() {
+            &self.input_buffer[self.cursor_pos - input_area_width()..self.cursor_pos]
+        } else {
+            self.input_buffer.as_str()
+        };
+        let input_paragraph = Paragraph::new(text)
             .block(input_block)
             .style(Style::default().fg(Color::Yellow));
         // 输入区域
@@ -186,27 +217,33 @@ impl Widget for &mut UI {
             .chat_history
             .iter()
             .map(|m| {
-                // 这里 -2 的原因是因为输入框中具有两侧的的 1px 边框
-                let area_width = input_area_area.width as usize - 2;
+                let area_width = chat_area_width();
                 let mut message_max_width = m.message.len();
+                // 如果是用户进行提问的消息，则将换行符改为 ',' ; 如果是机器人回答的消息，则照搬
+                let mut message = match m.sender {
+                    User => m.message.clone().replace("\n", ","),
+                    Bot => m.message.clone(),
+                };
                 // 对长文本进行插入换行符号
-                let mut message = m.message.clone();
+                let mut line = 1;
                 while message_max_width > area_width {
-                    message.insert(area_width, '\n');
+                    message.insert(area_width * line + line - 1, '\n');
                     message_max_width -= area_width;
+                    line += 1;
                 }
                 ChatMessage {
                     sender: m.sender.clone(),
                     message,
-                    timestamp: m.timestamp,
+                    date_time: m.date_time,
                 }
             })
             .map(|m| (&m).into())
             .collect();
         let chat_list = List::new(items)
             .block(chat_block)
-            .style(Style::default().fg(Color::White));
+            .style(Style::default().fg(Color::White))
+            .scroll_padding(10);
         // 聊天记录区域
-        chat_list.render(chat_area, buf);
+        Widget::render(chat_list, chat_area, buf);
     }
 }
